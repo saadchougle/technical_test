3ede644b60a834395e9ab23434b13b824d3aff29
Saad Chougle <Saad.Chougle@necsws.com>
2024-06-03 11:18:24 +0530

    Updated DB Subnet Group in the RDS terraform module

--- Terraform/modules/aws_rds/main.tf
+++ Terraform/modules/aws_rds/main.tf
@@ -1,4 +1,10 @@
 # Creating AWS RDS
+#Creating DB Subnet Group
+resource "aws_db_subnet_group" "rds_subnetgroup" {
+  name        = var.db_subnetgroup_name
+  subnet_ids  = var.db_subnet_ids
+}
+
 resource "aws_db_instance" "rds" {
   identifier             = lower("${var.db_name}-db")
   allocated_storage      = var.allocated_storage
@@ -10,7 +16,7 @@ resource "aws_db_instance" "rds" {
   username               = var.db_username
   password               = var.db_password
   vpc_security_group_ids = var.db_sg_id
-  db_subnet_group_name   = var.db_subnetgroup_name
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnetgroup.name
   multi_az               = var.multi_az 
   skip_final_snapshot    = true
   tags = {
